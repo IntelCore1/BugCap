6381728625b050d64004cc57cf7c7177a19500a9
IntelCore1 <105950320+IntelCore1@users.noreply.github.com>
2022-08-30 09:08:14 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,6 +1,6 @@
 var data = [
     { ScrumTeamName: 'Cha-Ching', score: 3},
-    { ScrumTeamName: 'INDHAN', score: 6},
+    { ScrumTeamName: 'INDHAN', score: 7},
     { ScrumTeamName: 'Ka-Chow', score: 1},
 ];
 
